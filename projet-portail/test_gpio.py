ad9e5422d57aa759d350a094ebad859b1e4b5477
MathildeRouland <matrouland@gmail.com>
2025-10-10 11:23:19 +0200

    feat: change library to lgpio

--- projet-portail/test_gpio.py
+++ projet-portail/test_gpio.py
@@ -1,34 +1,27 @@
-import RPi.GPIO as GPIO
+import lgpio
 import time
-import sys
+
+# Numéro de GPIO (BCM)
+PIN = 17
 
 # Fonction pour ouvrir le portail pendant X secondes
 def open_gate(duration=10):
-    # Configuration GPIO
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(17, GPIO.OUT)
+  # Ouvre la puce GPIO 0 (le contrôleur principal)
+    h = lgpio.gpiochip_open(0)
 
-    # Ouvrir le portail
+    # Déclare la broche en sortie
+    lgpio.gpio_claim_output(h, PIN)
+
     print("Ouverture du portail...")
-    GPIO.output(17, GPIO.HIGH)
+    lgpio.gpio_write(h, PIN, 1)
     time.sleep(duration)
 
-    # Fermer le portail
     print("Fermeture du portail...")
-    GPIO.output(17, GPIO.LOW)
+    lgpio.gpio_write(h, PIN, 0)
 
-    # Nettoyage
-    GPIO.cleanup()
+    # Libère la ressource
+    lgpio.gpiochip_close(h)
+    print("Test terminé.")
 
-    # Si le script est exécuté directement depuis la ligne de commande
 if __name__ == "__main__":
-    duration = 2  # valeur par défaut
-
-    # Récupérer la durée passée en argument
-    if len(sys.argv) > 1:
-        try:
-            duration = int(sys.argv[1])
-        except ValueError:
-            print("Argument non valide, utilisation de la valeur par d�faut (2s)")
-
-    open_gate(duration)
+    open_gate()
