ac97cf61fe9ef4a51965dc2e4f295adf482bd990
MathildeRouland <matrouland@gmail.com>
2025-10-21 17:33:12 +0200

    feat: add userEditType for update user

---
+++ projet-portail/src/Form/UserEditType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        /** @var User|null $currentUser */
+        $currentUser = $options['current_user'];
+
+        $builder
+           ->add('username', null, [
+                'label' => 'Nom d\'utilisateur',
+                'required' => true,
+            ])
+            ->add('email', null, [
+                'label' => 'Email',
+                'required' => true,
+            ])
+            ->add('roles', \Symfony\Component\Form\Extension\Core\Type\ChoiceType::class, [
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                    'Super Administrateur' => 'ROLE_SUPER_ADMIN',
+                ],
+                'multiple' => false,
+                'expanded' => false,
+                'label' => 'Rôle',
+                'mapped' => false,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'current_user' => null, 
+        ]);
+    }
+}
